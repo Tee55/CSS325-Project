34514cab584e31f66323a0877b6dfd0d6058033d
Tee55 <teerapaths@gmail.com>
2023-10-28 15:33:38 +0700

    test

--- main.py
+++ main.py
@@ -1,45 +1,86 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import pandas as pd
 import nltk
 nltk.download('stopwords')
-from nltk.corpus import stopwords
+
+def accuracy_function(tp, tn, fp, fn):
+    accuracy = (tp+tn) / (tp+tn+fp+fn)
+    return accuracy
+
+
+def precision_function(tp, fp):
+    precision = tp / (tp+fp)
+    return precision
+
+
+def recall_function(tp, fn):
+    recall = tp / (tp+fn)
+    return recall
+
+
+def confusion_matrix(truth, predicted):
+
+    true_positive = 0
+    true_negative = 0
+    false_positive = 0
+    false_negative = 0
+
+    for true, pred in zip(truth, predicted):
+
+        if true == '1' or 1:
+            if pred == true:
+                true_positive += 1
+            elif pred != true:
+                false_negative += 1
+
+        elif true == '0' or 1:
+            if pred == true:
+                true_negative += 1
+            elif pred != true:
+                false_positive += 1
+
+    accuracy = accuracy_function(true_positive, true_negative, false_positive, false_negative)
+    precision = precision_function(true_positive, false_positive)
+    recall = recall_function(true_positive, false_negative)
+
+    return accuracy, precision, recall
 
 df = pd.read_csv("./dataset/SQLiV3.csv")
+
+# Drop duplicate row
 df = df.drop_duplicates('Sentence')
-df = df.drop('Unnamed: 2', axis=1)
-df = df.drop('Unnamed: 3', axis=1)
+
+df = df.drop('Unnamed: 2', axis=1, errors='ignore')
+df = df.drop('Unnamed: 3', axis=1, errors='ignore')
+
+# Drop row with Nan
 df = df.dropna(how='any')
 
-train = df.sample(frac=1).reset_index(drop=True)
+print(df)
 
-train = df.iloc[:30000]
-test = df.iloc[30000:]
+# Reset index
+df = df.sample(frac=1).reset_index(drop=True)
+y = df['Label'].values
 
-from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer(ngram_range=(2, 2))
+#vectorizer = TfidfVectorizer(min_df=2, max_df=0.7, stop_words='english')
 
-countVectorizer = CountVectorizer(min_df=2, max_df=0.7, stop_words=stopwords.words('english'))
-t1 = countVectorizer.fit_transform(df['Sentence'].values.astype('U')).toarray()
-vocabulary = countVectorizer.get_feature_names_out()
-vectorizer = CountVectorizer(min_df=2, max_df=0.7, stop_words=stopwords.words('english'), vocabulary=vocabulary)
+count_matrix = vectorizer.fit_transform(df['Sentence'])
+for name in vectorizer.get_feature_names_out():
+    print(name)
 
-X_train = vectorizer.fit_transform(train['Sentence']).toarray()
-y_train = train['Label'].values
+X_train, X_test, y_train, y_test = train_test_split(count_matrix, y, test_size=0.2, random_state=42)
 
-X_test = vectorizer.fit_transform(test['Sentence']).toarray()
-y_test = test['Label'].values
 # (sentence_index, feature_index) count
+print(X_train.shape, X_test.shape)
 print(X_train)
 
 # Linear Regression
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_recall_fscore_support
-
-model = LogisticRegression(random_state=0).fit(X_train, y_train)
+model = LogisticRegression().fit(X_train, y_train)
 y_pred = model.predict(X_test)
-a = accuracy_score(y_test, y_pred)
-precision, recall, fscore, _ = precision_recall_fscore_support(y_test, y_pred, average='weighted')
-
-print("Accuracy : {:.4f}".format(a))
+accuracy, precision, recall = confusion_matrix(y_test, y_pred)
+print("Accuracy : {:.4f}".format(accuracy))
 print("Precision : {:.4f}".format(precision))
 print("Recall : {:.4f}".format(recall))
-print("F-Score : {:.4f}".format(fscore))
