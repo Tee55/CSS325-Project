efb6b439fd011dd8240b7ce1fd736193314c1565
Tee55 <teerapaths@gmail.com>
2023-11-18 19:40:25 +0700

    main

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
+os.environ["KERAS_BACKEND"] = "tensorflow"
+import tensorflow as tf
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.neighbors import KNeighborsClassifier
+
 import pandas as pd
+import numpy as np
 import nltk
 nltk.download('stopwords')
 
@@ -18,7 +25,6 @@ def precision_function(tp, fp):
 def recall_function(tp, fn):
     recall = tp / (tp+fn)
     return recall
-
 
 def confusion_matrix(truth, predicted):
 
@@ -47,40 +53,95 @@ def confusion_matrix(truth, predicted):
 
     return accuracy, precision, recall
 
-df = pd.read_csv("./dataset/SQLiV3.csv")
+def build_cnn():
 
-# Drop duplicate row
-df = df.drop_duplicates('Sentence')
+    model = tf.keras.Sequential(name="CNN")
 
-df = df.drop('Unnamed: 2', axis=1, errors='ignore')
-df = df.drop('Unnamed: 3', axis=1, errors='ignore')
+    model.add(tf.keras.layers.Input(shape=(4096, 1)))
 
-# Drop row with Nan
-df = df.dropna(how='any')
+    model.add(tf.keras.layers.Conv1D(filters=64, kernel_size=4, activation='relu'))
+    model.add(tf.keras.layers.MaxPooling1D(pool_size=2))
 
-print(df)
+    model.add(tf.keras.layers.Conv1D(filters=128, kernel_size=4, activation='relu'))
+    model.add(tf.keras.layers.MaxPooling1D(pool_size=2))
 
-# Reset index
-df = df.sample(frac=1).reset_index(drop=True)
-y = df['Label'].values
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(units=128, activation='relu'))
+    model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
-vectorizer = CountVectorizer(ngram_range=(2, 2))
-#vectorizer = TfidfVectorizer(min_df=2, max_df=0.7, stop_words='english')
+    print(model.summary())
 
-count_matrix = vectorizer.fit_transform(df['Sentence'])
-for name in vectorizer.get_feature_names_out():
-    print(name)
+    return model
 
-X_train, X_test, y_train, y_test = train_test_split(count_matrix, y, test_size=0.2, random_state=42)
+if __name__ == '__main__':
+    df = pd.read_csv("./dataset/SQLiV3.csv")
 
-# (sentence_index, feature_index) count
-print(X_train.shape, X_test.shape)
-print(X_train)
+    # Drop duplicate row
+    df = df.drop_duplicates('Sentence')
 
-# Linear Regression
-model = LogisticRegression().fit(X_train, y_train)
-y_pred = model.predict(X_test)
-accuracy, precision, recall = confusion_matrix(y_test, y_pred)
-print("Accuracy : {:.4f}".format(accuracy))
-print("Precision : {:.4f}".format(precision))
-print("Recall : {:.4f}".format(recall))
+    df = df.drop('Unnamed: 2', axis=1, errors='ignore')
+    df = df.drop('Unnamed: 3', axis=1, errors='ignore')
+
+    # Drop row with Nan
+    df = df.dropna(how='any')
+
+    # Drop row with incorrect label
+    df = df[(df['Label'] == '0') | (df['Label'] == '1')]
+
+    print(df.head())
+
+    # Reset index
+    df = df.sample(frac=1).reset_index(drop=True)
+    y = np.array([int(i) for i in df['Label'].values])
+
+    vectorizer = CountVectorizer(max_features=4096, ngram_range=(2, 2))
+    #vectorizer = TfidfVectorizer(min_df=2, max_df=0.7, stop_words='english')
+
+    count_matrix = vectorizer.fit_transform(df['Sentence']).toarray()
+
+    X_train, X_test, y_train, y_test = train_test_split(count_matrix, y, test_size=0.2, random_state=42)
+
+    # (sentence_index, feature_index) count
+    print("Train shape: {}".format(X_train.shape))
+    print("Test shape: {}".format(X_test.shape))
+
+    # Logistic Regression
+    model = LogisticRegression().fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    accuracy, precision, recall = confusion_matrix(y_test, y_pred)
+    print("=================Logistic Regression Result=================")
+    print("Accuracy : {:.4f}".format(accuracy))
+    print("Precision : {:.4f}".format(precision))
+    print("Recall : {:.4f}".format(recall))
+
+
+    knn = KNeighborsClassifier(n_neighbors=3)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    accuracy, precision, recall = confusion_matrix(y_test, y_pred)
+    print("=================KNN Result=================")
+    print("Accuracy : {:.4f}".format(accuracy))
+    print("Precision : {:.4f}".format(precision))
+    print("Recall : {:.4f}".format(recall))
+
+    X_train = X_train.reshape(-1, 4096, 1)
+    X_test = X_test.reshape(-1, 4096, 1)
+
+    # CNN shape
+    print("Train shape: {}".format(X_train.shape))
+    print("Test shape: {}".format(X_test.shape))
+
+    cnn = build_cnn()
+    cnn.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate=3e-4), metrics=['accuracy'])
+    cnn.fit(X_train, y_train, batch_size=10, epochs=5, validation_data=(X_test, y_test))
+
+    y_pred = cnn.predict(X_test)
+
+    # Turn sigmoid result to 0 or 1
+    y_pred = np.where(y_pred > 0.5, 1, 0)
+    
+    accuracy, precision, recall = confusion_matrix(y_test, y_pred)
+    print("=================CNN Result=================")
+    print("Accuracy : {:.4f}".format(accuracy))
+    print("Precision : {:.4f}".format(precision))
+    print("Recall : {:.4f}".format(recall))
